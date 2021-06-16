f13f660cb0fc288e597dcfd0a2d64a3eb9497353
Tsadiku Obolu <tsadiku@google.com>
2021-06-16 23:34:44 +0000

    name change mess

---
+++ my-app/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react'; 
+import ReactDOM, { render } from 'react-dom'; 
+import './index.css'; 
+
+class ButtonAndText extends React.Component {
+  constructor(props) {
+    super(props);  
+    this.state = {isToggleOn: true}; 
+    this.handleClick = this.handleClick.bind(this);   
+  } 
+  handleClick() {
+    this.setState(prevState => ({isToggleOn: !prevState.isToggleOn}))
+  }
+  render() {
+    return ( 
+      <div class="flex"> 
+        <div>
+          <button onClick={this.handleClick} type="button" class="btn">
+            Done!
+          </button>
+        </div> 
+        <div>
+          <div class="input-group">  
+          {this.state.isToggleOn ? <input type="text" value={this.props.name}/> : <form> <input type="text"/> </form>}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}   
+
+const arr = [<ButtonAndText />, <ButtonAndText />, <ButtonAndText />, <ButtonAndText />] 
+const listItems = arr.map((arr) => <li>{arr}</li>) 
+ReactDOM.render(<ul>{listItems}</ul>, document.getElementById('root'));
+// class UserInputEx extends React.Component {
+//   render() {
+//     return (
+//       <form>
+//         <input type="text"/>
+//       </form>
+//     )
+//   }
+// }  
+
